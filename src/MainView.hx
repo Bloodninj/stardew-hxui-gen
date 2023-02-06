adbc1a533964ca56760eae8dc6fce40f9f28414a
Matt Slater <github@mslater.fastmail.com>
2023-02-06 13:09:44 +0000

    Add text editing

--- src/MainView.hx
+++ src/MainView.hx
@@ -1,5 +1,6 @@
 package ;
 
+import haxe.ui.data.ArrayDataSource;
 import haxe.ui.events.UIEvent;
 import sys.io.File;
 import haxe.ui.containers.VBox;
@@ -30,10 +31,21 @@ class MainView extends VBox {
             trace('${randReaction} (${npc.reactions[randReaction].id})');
             trace(npc.reactions[randReaction].specialResponses != null ? npc.reactions[randReaction].specialResponses.beforeMovie.text : "[nothing]");
 
-            characterDropdown.onChange = characterDropdown_onChange;
+            // characterDropdown.onChange = characterDropdown_onChange;
+            // Add options to character dropdown
             for (r in originalReactions) {
                 characterDropdown.dataSource.add(r.npcName);
             }
+            // Add options to reaction tag dropdown
+            var reactionTags = new ArrayDataSource<String>();
+            reactionTags.add("Love");
+            reactionTags.add("Like");
+            reactionTags.add("Dislike");
+            reactionTags.add("Seen (Love)");
+            reactionTags.add("Seen (Like)");
+            reactionTags.add("Seen (Dislike)");
+            
+            reactionResponseDropdown.dataSource = reactionTags;
 
         } catch (e) {
             trace(e.message);
@@ -42,25 +54,28 @@ class MainView extends VBox {
         trace(reader.errors);
     }
 
-    function characterDropdown_onChange(e:UIEvent):Void {
+    @:bind(characterDropdown, UIEvent.CHANGE)
+    function characterDropdown_onChange(e:haxe.ui.events.UIEvent) {
         try {
             var charName:String = e.target.value;
-            for (r in originalReactions) {
-                if (r.npcName == charName) {
-                    npcPortrait.resource = 'file://assets/images/portraits/portrait-${r.npcName.toLowerCase()}.png';
-                    // npcNameLabel.text = r.npcName;
+            for (npc in originalReactions) {
+                if (npc.npcName == charName) {
+                    npcPortrait.resource = 'file://assets/images/portraits/portrait-${npc.npcName.toLowerCase()}.png';
+                    // npcNameLabel.text = npc.npcName;
 
                     // Set up reactions
                     reactionDropdown.disabled = false;
                     reactionDropdown.dataSource.clear();
-                    for (react in r.reactions) {
+                    var source = new ArrayDataSource<String>();
+                    for (react in npc.reactions) {
                         trace(react.id);
-                        reactionDropdown.dataSource.add(react.id);
+                        source.add('${react.tag} (${react.id}');
                     }
                     trace('Data:');
                     for (i in 0...reactionDropdown.dataSource.size) {
                         trace(reactionDropdown.dataSource.get(i));
                     }
+                    reactionDropdown.dataSource = source;
                     break;
                 }
             }
@@ -69,6 +84,77 @@ class MainView extends VBox {
             trace(e.details);
             npcPortrait.resource = 'file://assets/images/AnsweringMachine.png';
             // npcNameLabel.text = "ERROR!";
+        }
+    }
+
+    @:bind(reactionDropdown, UIEvent.CHANGE)
+    function reactionDropdown_onChange(e:haxe.ui.events.UIEvent) {
+        var reactionID:String = e.target.value;
+        var currentNPC:String = characterDropdown.value;
+        // search for reaction
+        for (npc in originalReactions) {
+            if (npc.npcName == currentNPC) {
+                var reactList = npc.reactions.filter((item) -> {
+                    return ('${item.tag} (${item.id}' == reactionID);
+                });
+                if (reactList.length > 0) {
+                    var r = reactList[0];
+                    // we found the reaction, so enable editing controls
+                    reactionResponseDropdown.disabled = false;
+                    reactionResponseDropdown.selectedIndex = switch(r.response) {
+                        case "love": 0;
+                        case "like": 1;
+                        case "dislike": 2;
+                        case "seen_love": 3;
+                        case "seen_like": 4;
+                        case "seen_dislike": 5;
+                        default: 0;
+                    }
+
+                    // Fill in reaction text, if it exists
+                    if (r.specialResponses != null) {
+                        if (r.specialResponses.beforeMovie != null) {
+                            beforeMovieTab.disabled = false;
+                            beforeMovieText.disabled = false;
+                            beforeMovieText.text = r.specialResponses.beforeMovie.text;
+                        } else {
+                            beforeMovieTab.disabled = true;
+                            beforeMovieText.disabled = true;
+                            beforeMovieText.text = "";
+                        }
+                        if (r.specialResponses.duringMovie != null) {
+                            duringMovieTab.disabled = false;
+                            duringMovieText.disabled = false;
+                            duringMovieText.text = r.specialResponses.duringMovie.text;
+                        } else {
+                            duringMovieTab.disabled = true;
+                            duringMovieText.disabled = true;
+                            duringMovieText.text = "";
+                        }
+                        if (r.specialResponses.afterMovie != null) {
+                            afterMovieTab.disabled = false;
+                            afterMovieText.disabled = false;
+                            afterMovieText.text = r.specialResponses.afterMovie.text;
+                        } else {
+                            afterMovieTab.disabled = true;
+                            afterMovieText.disabled = true;
+                            afterMovieText.text = "";
+                        }
+                    } else {
+                        // No special responses for this reaction, so disable text editing controls
+                        beforeMovieTab.disabled = true;
+                            beforeMovieText.disabled = true;
+                            beforeMovieText.text = "";
+                            duringMovieTab.disabled = true;
+                            duringMovieText.disabled = true;
+                            duringMovieText.text = "";
+                            afterMovieTab.disabled = true;
+                            afterMovieText.disabled = true;
+                            afterMovieText.text = "";
+                    }
+                }
+            }
+            break;
         }
     }
 
